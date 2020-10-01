51fa7e58b5ecf11cc0ed0676914a5dfb29091014
Marise Hayashi <limitedeternity@users.noreply.github.com>
2020-10-01 10:26:01 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,16 +1,23 @@
-<html>
-    <head>
-        <title>bdb64</title>
-    </head>
-    <body>
-        <p>
-        <?php 
-            if (isset($_GET["decode"])) {
-                echo base64_decode($_GET["decode"]);
-            } elseif (isset($_GET["encode"])) {
-                echo base64_encode($_GET["encode"]);
-            }
-        ?>
-        </p>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />    
+    <title>bdb64</title>
+    <style>
+      h2 {
+        font-size:3vw;
+        word-wrap: break-word;
+      }
+    </style>
+  </head>
+  <body>
+  <?php
+      if (isset($_GET["decode"])) {
+        echo "<h2>" . htmlspecialchars(base64_decode($_GET["decode"])) . "</h2>";
+      } elseif (isset($_GET["encode"])) {
+        echo "<h2>" . htmlspecialchars(base64_encode($_GET["encode"])) . "</h2>";
+      }
+  ?></body>
 </html>
